c5ef066e8c0fff753d4c3cd5fc5f8ed0a961849d
Valle12 <valentinkaczmarek@yahoo.de>
2024-08-20 20:28:17 +0200

    BCE-12 update icons type

--- dev/types.ts
+++ dev/types.ts
@@ -5,22 +5,49 @@ export type OnlyKnown<T> = {
     ? never
     : K]: T[K];
 };
+
+// Manifest
 export type CleanManifest = OnlyKnown<chrome.runtime.ManifestV3>;
+
+export type OmitManifest = Omit<
+  CleanManifest,
+  "content_scripts" | "icons" | "action"
+>;
+
 export type ContentScript = Omit<
   NonNullable<CleanManifest["content_scripts"]>[number],
   "js"
 >;
-export type OmitManifest = Omit<CleanManifest, "content_scripts">;
+
 export type CustomContentScript = ContentScript & {
   run_at?: "document_start" | "document_end" | "document_idle";
   matches?: string[] | ["<all_urls>"];
   ts?: string[];
 };
+
+export type Icons = Partial<{
+  16: string;
+  32: string;
+  48: string;
+  128: string;
+  [key: string]: string;
+}>;
+
+export type Action = Omit<NonNullable<CleanManifest["action"]>, "default_icon">;
+
+export type CustomAction = Action & {
+  default_icon?: Icons | string;
+};
+
 export type FullManifest = OmitManifest & {
   content_scripts?: CustomContentScript[];
+  icons?: Icons | string;
+  action?: CustomAction;
 };
+
 export type Manifest = Omit<FullManifest, "manifest_version">;
 
+// Build types
 export type HTMLType = {
   originalURL: string;
   distURL?: string;
