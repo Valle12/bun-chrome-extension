85b612b0725f69932b87fc0a9585cabad10dcec0
Valle12 <valentinkaczmarek@yahoo.de>
2026-01-30 14:50:34 +0100

    BCE-2 higer timeout and more retries

--- dev/test/integration.test.ts
+++ dev/test/integration.test.ts
@@ -24,78 +24,87 @@ describe("bce integration", () => {
     await rm(run, { recursive: true, force: true });
   });
 
-  test("test if watching and reloading works as expected", async () => {
-    expect.assertions(12);
+  test(
+    "test if watching and reloading works as expected",
+    async () => {
+      expect.assertions(12);
 
-    await build(run);
+      await build(run);
 
-    const proc = Bun.spawn({
-      cmd: ["bun", resolve(run, "bce.js"), "--dev"],
-      cwd,
-      env: { ...process.env, LOCAL: "true" },
-      stdout: "ignore",
-      stderr: "ignore",
-      stdin: "pipe",
-    });
+      const proc = Bun.spawn({
+        cmd: ["bun", resolve(run, "bce.js"), "--dev"],
+        cwd,
+        env: { ...process.env, LOCAL: "true" },
+        stdout: "ignore",
+        stderr: "ignore",
+        stdin: "pipe",
+      });
 
-    const watcher = watch(cwd, {
-      awaitWriteFinish: {
-        stabilityThreshold: 100,
-        pollInterval: 10,
-      },
-      ignoreInitial: true,
-    });
+      const watcher = watch(cwd, {
+        awaitWriteFinish: {
+          stabilityThreshold: 100,
+          pollInterval: 10,
+        },
+        ignoreInitial: true,
+      });
 
-    let counter = 0;
-    watcher.on("all", async (_event, filename) => {
-      if (!filename.includes("manifest.json")) return;
-      counter++;
-      new Connection().connect();
+      let counter = 0;
+      watcher.on("all", async (_event, filename) => {
+        if (!filename.includes("manifest.json")) return;
+        counter++;
+        new Connection().connect();
 
-      const manifest = await Bun.file(resolve(dist, "manifest.json")).text();
-      expect(manifest).toMatchSnapshot();
+        const manifest = await Bun.file(resolve(dist, "manifest.json")).text();
+        expect(manifest).toMatchSnapshot();
 
-      const solaceDist = await Bun.file(resolve(dist, "src/solace.js")).text();
-      expect(solaceDist).not.toContain("export");
-      expect(solaceDist).toMatchSnapshot();
+        const solaceDist = await Bun.file(
+          resolve(dist, "src/solace.js"),
+        ).text();
+        expect(solaceDist).not.toContain("export");
+        expect(solaceDist).toMatchSnapshot();
 
-      if (counter === 1) {
-        const solace = Bun.file(resolve(cwd, "src/solace.ts"));
-        const content = await solace.text();
-        await Bun.write(solace, content);
-      } else if (counter === 2) {
-        const content = originalManifestContent.replaceAll(
-          'ts: ["src/solace.ts"]',
-          "ts: []"
-        );
-        await Bun.write(manifestPath, content);
-      } else if (counter === 3) {
-        watcher.close();
-      }
-    });
+        if (counter === 1) {
+          const solace = Bun.file(resolve(cwd, "src/solace.ts"));
+          const content = await solace.text();
+          await Bun.write(solace, content);
+        } else if (counter === 2) {
+          const content = originalManifestContent.replaceAll(
+            'ts: ["src/solace.ts"]',
+            "ts: []",
+          );
+          await Bun.write(manifestPath, content);
+        } else if (counter === 3) {
+          watcher.close();
+        }
+      });
 
-    const distWatcher = watch(dist, {
-      awaitWriteFinish: {
-        stabilityThreshold: 100,
-        pollInterval: 10,
-      },
-      ignoreInitial: true,
-    });
+      const distWatcher = watch(dist, {
+        awaitWriteFinish: {
+          stabilityThreshold: 100,
+          pollInterval: 10,
+        },
+        ignoreInitial: true,
+      });
 
-    let manifestCounter = 0;
-    distWatcher.on("all", async (_event, filename) => {
-      if (!filename.endsWith("manifest.json")) return;
-      // The third time solace.js is not in manifest.json, but dist still contains unused files until process restarted
-      expect(
-        await Bun.file(resolve(dist, "src/solace.js")).exists()
-      ).toBeTrue();
-      manifestCounter++;
-      if (manifestCounter === 3) {
-        distWatcher.close();
-        proc.stdin.write("\u0003"); // Simulate CTRL + C
-      }
-    });
+      let manifestCounter = 0;
+      distWatcher.on("all", async (_event, filename) => {
+        if (!filename.endsWith("manifest.json")) return;
+        // The third time solace.js is not in manifest.json, but dist still contains unused files until process restarted
+        expect(
+          await Bun.file(resolve(dist, "src/solace.js")).exists(),
+        ).toBeTrue();
+        manifestCounter++;
+        if (manifestCounter === 3) {
+          distWatcher.close();
+          proc.stdin.write("\u0003"); // Simulate CTRL + C
+        }
+      });
 
-    await proc.exited;
-  });
+      await proc.exited;
+    },
+    {
+      timeout: 20000,
+      retry: 3,
+    },
+  );
 });
